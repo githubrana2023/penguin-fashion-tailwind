cc4408943cbab803011744f2a3d82858c23f7dca
githubrana2023 <rtrana2023@gmail.com>
2023-01-21 22:58:55 +0300

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "576px",
+      md: "960px",
+      lg: "1440px",
+    },
     extend: {},
   },
   plugins: [],
